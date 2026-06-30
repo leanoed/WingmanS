ed4c38f6777d5cbb1efab211067ee493c53ad561
leanoed <bubny1337@gmail.com>
2026-07-01 03:58:27 +0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -91,8 +91,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#FF4654",
+      secondary: "#111823",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
